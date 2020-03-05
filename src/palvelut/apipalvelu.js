88a75f124805221a55175daf23fe4897485bf3ec
OskariAuvinen <55886851+OskariAuvinen@users.noreply.github.com>
2020-03-05 10:51:23 +0200

    ?

--- src/palvelut/apipalvelu.js
+++ src/palvelut/apipalvelu.js
@@ -5,8 +5,7 @@ import axios from 'axios';
 import KysymyksetComponent from '../Komponentit/KysymyksetComponent';
 
 let url = 'http://localhost:8000/api/quiz';
-const linkki = "/kysymykset";
-let n;
+
 
 export function tarkista(oikein) {
     if(oikein) {
@@ -25,7 +24,6 @@ export function kirjaudu(nimi) {
     } else {
         //lähetä nimi palvelimelle ja siirry pelisivulle
         postKayttaja({nimi: nimi});
-        console.log("yritertään redirect");
         sessionStorage.clear();
         sessionStorage.setItem("1", nimi);
     return(  window.location.href="/kysymykset" )
@@ -38,18 +36,3 @@ async function postKayttaja(nimi) {
         return res.data;
     });
  }
-
-export const haeHighScore = async () => {
-     let scoret = await axios.get(`${url}/pisteet`)
-     return scoret.data;
- } 
-
- export const haeAllTimeHighScore = async () => {
-    let scoret = await axios.get(`${url}/kaikkipisteet`)
-    return scoret.data;
-} 
-
-export const haeKuukaudenTimeHighScore = async (kk, yyyy) => {
-    let scoret = await axios.get(`${url}/pisteet/${kk}/${yyyy}`)
-    return scoret.data;
-} 
