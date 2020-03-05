53abb96fcdaa8a2c87b72e6eb6c1113e89b88f2b
kanlaura <59911290+kanlaura@users.noreply.github.com>
2020-03-05 17:12:43 +0200

    maxScores working

---
+++ src/Hooks/Top5AllTime.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
+export default function Top5AllTime(props) {
+    const [score, setScore] = useState({
+        top: props.top5,
+        kk: parseInt(moment(new Date).format('MM')),
+        yyyy: parseInt(moment(new Date).format('YYYY'))
+    });
+
+    // Päivittää highScoren käynnistämällä yläpuolella olevan function--Laura
+    useEffect(() => {
+        setScore({ ...score, top: props.top5 })
+
+    }, [props.top5])
+
+    return (
+        <div>
+            <h1>Top-5 käyttäjät</h1>
+            <hr />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Käyttäjä</th><th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {score.top.map(t => (<tr key={t.kayttaja_id}><td>{t.nimi}</td><td>{t.maximit}</td></tr>))}
+                </tbody>
+            </table>
+        </div>
+    );
+
+}
