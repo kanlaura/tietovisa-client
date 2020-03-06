ee68f938748d5c8b10c2484321f5613bc78e3b3c
Niina Siitari <siitari.niina@gmail.com>
2020-03-06 11:36:52 +0200

    Frontin testikoodit toimivat.

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+//Testaus toimii, -Niina
+test('renderöi HighScore linkin', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkki = getByText(/HighScore/i);
+  expect(linkki).toBeInTheDocument();
+});
+
+test('renderoi aloituslomakkeen', () => {
+  const { getByPlaceholderText } = render(<App />);
+  const button = getByPlaceholderText(/nimimerkki/i);
+  expect(button).toBeInTheDocument();
 });
